8bcf72805f75768668a1511979f37bbf3ad9cef6
Gabriel Pitanga <gpitanga13@gmail.com>
2025-05-08 10:30:54 -0300

    exercicio de POO - Introdução

---
+++ POO/poo_2.py
@@ -0,0 +1,17 @@
+class PickNum:
+    def __init__(self, num):
+        self.num = num
+        self.par_impar()
+    
+    def par_impar(self,):
+        if self.num % 2 == 0:
+            self.parimpar = "Par"
+        else:
+            self.parimpar = "Impar"
+
+    def show_num(self):
+        print(f"numero = {self.num}, é: {self.parimpar}")
+
+
+picked_num = PickNum(2)
+picked_num.show_num()
